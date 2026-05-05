9f2279daae00974b5167f77d7703ea1d2eacdb6c
akash30504 <raamihisara@students.nsbm.ac.lk>
2026-05-05 21:23:13 +0530

    Create email_config.py

---
+++ data/email_config.py
@@ -0,0 +1,3 @@
+EMAIL_SENDER   = "akashmihisara21@gmail.com"   # your Gmail address
+EMAIL_PASSWORD = "sdgnqsygfrvgqmtd"        # your 16-char App Password (no spaces)
+EMAIL_RECEIVER = "akashmihisara21@gmail.com"    # where to receive alerts (can be same)
