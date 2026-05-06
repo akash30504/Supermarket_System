b3a12c6cf91593b792b965afefe5deb063f2cdc0
akash30504 <140973075+akash30504@users.noreply.github.com>
2026-05-06 11:46:50 +0530

    Remove email configuration from email_config.py
    Removed sensitive email configuration details.

--- data/email_config.py
+++ data/email_config.py
@@ -1,3 +1,1 @@
-EMAIL_SENDER   = "akashmihisara21@gmail.com"   # your Gmail address
-EMAIL_PASSWORD = "sdgnqsygfrvgqmtd"        # your 16-char App Password (no spaces)
-EMAIL_RECEIVER = "akashmihisara21@gmail.com"    # where to receive alerts (can be same)
+
