0ff18fa0d851b305d298f5849cfef0dfef3ee1c5
akash30504 <raamihisara@students.nsbm.ac.lk>
2026-05-06 15:57:12 +0530

    Delete email_config.py

--- data/email_config.py
+++
@@ -1,3 +0,0 @@
-EMAIL_SENDER   = "akashmihisara21@gmail.com"   # your Gmail address
-EMAIL_PASSWORD = "sdgnqsygfrvgqmtd"        # your 16-char App Password (no spaces)
-EMAIL_RECEIVER = "akashmihisara21@gmail.com"    # where to receive alerts (can be same)
